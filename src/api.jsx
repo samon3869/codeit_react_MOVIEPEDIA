ba35ff549360e8c14ff7a74e22516a058b99b32b
samon3869 <68182398+samon3869@users.noreply.github.com>
2025-06-09 01:48:10 +0000

    cancel make error, remain error message

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,9 @@
 export async function getReviews({order = 'createdAt', offset = 0, limit = 6}) {
-    throw new Error('버그가 아니라 기능입니다');
     const query = `order=${order}&offset=${offset}&limit=${limit}`;
     const response = await fetch(`https://learn.codeit.kr/api/film-reviews?${query}`);
+    if (!response.ok) {
+        throw new Error('리뷰를 불러오는데 실패했습니다');
+    }
     const body = await response.json();
     return body;
 }
