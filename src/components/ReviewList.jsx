0896daba338bad8e8835407f3de0868dcfb65c78
samon3869 <samon3869@naver.com>
2025-06-14 07:38:13 +0000

    feat/star-point-component

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,3 +1,4 @@
+import Rating from './Raiting';
 import './ReviewList.css';
 
 
@@ -15,7 +16,7 @@ function ReviewListItem({ item, onDelete }) {
             <img src={item.imgUrl} alt={item.title} className="ReviewListItem-img" />
             <div>
                 <h1>{item.title}</h1>
-                <p>{item.rating}</p>
+                <Rating value = {item.rating} />
                 <p>{formatDate(item.createdAt)}</p>
                 <p>{item.content}</p>
                 <button onClick={handleDeleteClick}>삭제</button>
